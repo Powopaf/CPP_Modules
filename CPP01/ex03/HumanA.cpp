1261db3b0341ef1cd4fb8453edbbdbb934eab1bd
Pierre-Antoine Fournier <pifourni@f6r4s8.clusters.42paris.fr>
2026-03-02 14:14:17 +0100

    better constructor

--- CPP01/ex03/HumanA.cpp
+++ CPP01/ex03/HumanA.cpp
@@ -1,7 +1,9 @@
 #include "HumanA.hpp"
 #include <iostream>
 
-HumanA::HumanA(std::string name, Weapon& weapon) : name(name), weapon(weapon) {}
+HumanA::HumanA(std::string name, Weapon& weapon) : weapon(weapon) {
+	this->name = name;
+}
 
 void HumanA::attack() {
 	std::cout << this->name << " attacks with their " << this->weapon.getType() << std::endl;
