fd968c8feb3bebc8f87d8e93c606b6389f8c7377
pifourni <pifourni@f1br8s16.paris.42.school>
2026-08-13 13:50:50 +0200

    finish cpp9

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -6,12 +6,20 @@ static std::vector<int> fusionV(std::vector<int> a, std::vector<int> b) {
 	if (b.size() <= 0)
 		return a;
 	if (a[0] <= b[0]) {
-		a.insert(a.end(), b.begin(), b.end());
-		return a;
+		std::vector<int> restA(a.begin() + 1, a.end());
+		std::vector<int> merged;
+		merged.push_back(a[0]);
+		std::vector<int> tail = fusionV(restA, b);
+		merged.insert(merged.end(), tail.begin(), tail.end());
+		return merged;
 	}
 	else {
-		b.insert(b.end(), a.begin(), a.end());
-		return b;
+		std::vector<int> restB(b.begin() + 1, b.end());
+		std::vector<int> merged;
+		merged.push_back(b[0]);
+		std::vector<int> tail = fusionV(a, restB);
+		merged.insert(merged.end(), tail.begin(), tail.end());
+		return merged;
 	}
 }
 
@@ -27,9 +35,35 @@ std::vector<int> PmergeMe::sortVect(std::vector<int> v) {
 }
 
 static std::deque<int> fusionQ(std::deque<int> a, std::deque<int> b) {
-	
+	if (a.size() <= 0)
+		return a;
+	if (b .size() <= 0)
+		return b;
+	if (a[0] <= b[0]) {
+		std::deque<int> restA(a.begin() + 1, a.end());
+		std::deque<int> merged;
+		merged.push_back(a[0]);
+		std::deque<int> tail = fusionQ(restA, b);
+		merged.insert(merged.end(), tail.begin(), tail.end());
+		return merged;
+	}
+	else {
+		std::deque<int> restB(b.begin() + 1, b.end());
+		std::deque<int> merged;
+		merged.push_back(b[0]);
+		std::deque<int> tail = fusionQ(a, restB);
+		merged.insert(merged.end(), tail.begin(), tail.end());
+		return merged;
+	}
 }
 
 std::deque<int> PmergeMe::sortQueue(std::deque<int> q) {
-	
+	if (q.size() <= 1)
+		return q;
+	else {
+		std::deque<int>::iterator mid = q.begin() + q.size() / 2;
+		std::deque<int> left(q.begin(), mid);
+		std::deque<int> right(mid, q.end());
+		return fusionQ(sortQueue(left), sortQueue(right));
+	}
 }
--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -1,6 +1,8 @@
 #include "PmergeMe.hpp"
 #include <cctype>
 #include <cstdlib>
+#include <ctime>
+#include <iomanip>
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -49,6 +51,24 @@ static std::deque<int> init_queue(const std::string& nums) {
 	return values;
 }
 
+static void printValues(const std::vector<int>& values) {
+    for (std::vector<int>::size_type i = 0; i < values.size(); ++i) {
+        std::cout << values[i];
+        if (i + 1 < values.size())
+            std::cout << ' ';
+    }
+    std::cout << std::endl;
+}
+
+static void printValues(const std::deque<int>& values) {
+    for (std::deque<int>::size_type i = 0; i < values.size(); ++i) {
+        std::cout << values[i];
+        if (i + 1 < values.size())
+            std::cout << ' ';
+    }
+    std::cout << std::endl;
+}
+
 int main(int argc, char** argv) {
     if (argc < 2) {
         std::cerr << "Error: no arg input need 1 at least" << std::endl;
@@ -57,5 +77,30 @@ int main(int argc, char** argv) {
     std::string numbers = createNumbers(argv, argc);
 	std::vector<int> vect = init_vect(numbers);
 	std::deque<int> queue = init_queue(numbers);
+
+    std::cout << "Before: " << numbers << std::endl;
+
+    clock_t vectStart = clock();
+	std::vector<int> sortedVect = PmergeMe::sortVect(vect);
+    clock_t vectEnd = clock();
+
+    clock_t queueStart = clock();
+	std::deque<int> sortedQueue = PmergeMe::sortQueue(queue);
+    clock_t queueEnd = clock();
+
+    std::cout << "After: ";
+    printValues(sortedVect);
+    std::cout << "After: ";
+    printValues(sortedQueue);
+
+    double vectTime = static_cast<double>(vectEnd - vectStart) * 1000000.0 / CLOCKS_PER_SEC;
+    double queueTime = static_cast<double>(queueEnd - queueStart) * 1000000.0 / CLOCKS_PER_SEC;
+
+    std::cout << std::fixed << std::setprecision(5);
+    std::cout << "Time to process a range of " << vect.size()
+              << " elements with std::vector : " << vectTime << " us" << std::endl;
+    std::cout << "Time to process a range of " << queue.size()
+              << " elements with std::deque : " << queueTime << " us" << std::endl;
+
 	return 0;
 }
